43b352e277a8663b008a0caf4720d1ff777a3928
AKSRathor <98573681+AKSRathor@users.noreply.github.com>
2023-09-20 00:46:35 +0530

    Update Notification.jsx

--- src/Components/Dash/Notification/Notification.jsx
+++ src/Components/Dash/Notification/Notification.jsx
@@ -6,22 +6,22 @@ import dash from "../Dashboard.module.css"
 const NotiInf = [
     {
         topic: "Earphones booking confirmed ",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis veniam cupiditate temporibus autem consequuntur, sequi ullam eius maxime fuga ab velit, corrupti, exercitationem error? Perspiciatis iste minima error ut quaerat!",
+        desc: "our premium replacement for your damaged earphones, the Samsung Earbuds. These cutting-edge earbuds offer an unparalleled listening experience with crystal-clear sound quality and superior comfort. Here's why you should make the switch today:",
 
     },
     {
         topic: "Product 5 updates",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis veniam cupiditate temporibus autem consequuntur, sequi ullam eius maxime fuga ab velit, corrupti, exercitationem error? Perspiciatis iste minima error ut quaerat!",
+        desc: "Introducing the Xiaomi Restoration Project - your chance to breathe new life into a once-loved device. Here's why you should take the plunge: Affordable Tech Revival, Creative Freedom, Eco-Friendly Choice, Spare Parts Galore",
 
     },
     {
         topic: "Samsung Product Booking failed",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis veniam cupiditate temporibus autem consequuntur, sequi ullam eius maxime fuga ab velit, corrupti, exercitationem error? Perspiciatis iste minima error ut quaerat!",
+        desc: "Looking to upgrade your kitchen without breaking the bank? Say hello to the ultimate DIY project – our one-of-a-kind damaged Samsung fridge! This appliance offers an exciting opportunity to unleash your inner handyman or woman while saving big",
 
     },
     {
         topic: "Boat watch sold successfully",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis veniam cupiditate temporibus autem consequuntur, sequi ullam eius maxime fuga ab velit, corrupti, exercitationem error? Perspiciatis iste minima error ut quaerat!",
+        desc: "Have you ever dreamt of becoming a watchmaker, crafting timepieces with precision and passion? Now's your chance to embark on a horological adventure with our one-of-a-kind damaged Boat watch. This is not just a timepiece; it's a canvas for your creativity. ",
 
     }
 ]
